08b1226a6dee09e188ecb5a3f4e1f06251a276b8
Michal Kubecek <mkubecek@suse.cz>
2020-03-06 18:05:45 +0100

    netlink: support for pretty printing netlink messages
    To improve message reporting and debugging, add support for displaying
    netlink messages in human readable form, e.g.
    
        # ethtool --debug 0x10 -s eth0 msglvl drv on foo on probe off
        netlink error: bit name not found
        offending message and attribute:
            ETHTOOL_MSG_DEBUG_SET
                ETHTOOL_A_DEBUG_HEADER
                    ETHTOOL_A_HEADER_DEV_NAME = "eth0"
                ETHTOOL_A_DEBUG_MSGMASK
                    ETHTOOL_A_BITSET_BITS
                        ETHTOOL_A_BITSET_BITS_BIT
                            ETHTOOL_A_BITSET_BIT_NAME = "drv"
                            ETHTOOL_A_BITSET_BIT_VALUE = true
                        ETHTOOL_A_BITSET_BITS_BIT
        ===>                ETHTOOL_A_BITSET_BIT_NAME = "foo"
                            ETHTOOL_A_BITSET_BIT_VALUE = true
                        ETHTOOL_A_BITSET_BITS_BIT
                            ETHTOOL_A_BITSET_BIT_NAME = "probe"
    
    This commit only adds support for parsing and displaying a message and
    (optionally) highlighting an attribute on given offset (for extack error
    reporting). To actually use it, one also needs message descriptions, i.e.
    mapping of netlink attribute types to their symbolic names and payload
    formats (depending on context).
    
    Signed-off-by: Michal Kubecek <mkubecek@suse.cz>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

---
+++ netlink/prettymsg.h
@@ -0,0 +1,102 @@
+/*
+ * prettymsg.h - human readable message dump
+ *
+ * Support for pretty print of an ethtool netlink message
+ */
+
+#ifndef ETHTOOL_NETLINK_PRETTYMSG_H__
+#define ETHTOOL_NETLINK_PRETTYMSG_H__
+
+#include <linux/netlink.h>
+
+/* data structures for message format descriptions */
+
+enum pretty_nla_format {
+	NLA_INVALID,
+	NLA_BINARY,
+	NLA_U8,
+	NLA_U16,
+	NLA_U32,
+	NLA_X8,
+	NLA_X16,
+	NLA_X32,
+	NLA_S8,
+	NLA_S16,
+	NLA_S32,
+	NLA_STRING,
+	NLA_FLAG,
+	NLA_BOOL,
+	NLA_NESTED,
+	NLA_ARRAY,
+};
+
+struct pretty_nla_desc {
+	enum pretty_nla_format		format;
+	const char			*name;
+	const struct pretty_nla_desc	*children;
+	unsigned int			n_children;
+};
+
+struct pretty_nlmsg_desc {
+	const char			*name;
+	const struct pretty_nla_desc	*attrs;
+	unsigned int			n_attrs;
+};
+
+/* helper macros for message format descriptions */
+
+#define NLATTR_DESC(_name, _fmt) \
+	[_name] = { \
+		.format = _fmt, \
+		.name = #_name, \
+	}
+
+#define NLATTR_DESC_INVALID(_name)	NLATTR_DESC(_name, NLA_INVALID)
+#define NLATTR_DESC_U8(_name)		NLATTR_DESC(_name, NLA_U8)
+#define NLATTR_DESC_U16(_name)		NLATTR_DESC(_name, NLA_U16)
+#define NLATTR_DESC_U32(_name)		NLATTR_DESC(_name, NLA_U32)
+#define NLATTR_DESC_X8(_name)		NLATTR_DESC(_name, NLA_X8)
+#define NLATTR_DESC_X16(_name)		NLATTR_DESC(_name, NLA_X16)
+#define NLATTR_DESC_X32(_name)		NLATTR_DESC(_name, NLA_X32)
+#define NLATTR_DESC_S8(_name)		NLATTR_DESC(_name, NLA_U8)
+#define NLATTR_DESC_S16(_name)		NLATTR_DESC(_name, NLA_U16)
+#define NLATTR_DESC_S32(_name)		NLATTR_DESC(_name, NLA_U32)
+#define NLATTR_DESC_STRING(_name)	NLATTR_DESC(_name, NLA_STRING)
+#define NLATTR_DESC_FLAG(_name)		NLATTR_DESC(_name, NLA_FLAG)
+#define NLATTR_DESC_BOOL(_name)		NLATTR_DESC(_name, NLA_BOOL)
+#define NLATTR_DESC_BINARY(_name)	NLATTR_DESC(_name, NLA_BINARY)
+
+#define NLATTR_DESC_NESTED(_name, _children_desc) \
+	[_name] = { \
+		.format = NLA_NESTED, \
+		.name = #_name, \
+		.children = __ ## _children_desc ## _desc, \
+		.n_children = ARRAY_SIZE(__ ## _children_desc ## _desc), \
+	}
+#define NLATTR_DESC_NESTED_NODESC(_name) NLATTR_DESC(_name, NLA_NESTED)
+#define NLATTR_DESC_ARRAY(_name, _children_desc) \
+	[_name] = { \
+		.format = NLA_ARRAY, \
+		.name = #_name, \
+		.children = __ ## _children_desc ## _desc, \
+		.n_children = 1, \
+	}
+
+#define NLMSG_DESC(_name, _attrs) \
+	[_name] = { \
+		.name = #_name, \
+		.attrs = __ ## _attrs ## _desc, \
+		.n_attrs = ARRAY_SIZE(__ ## _attrs ## _desc), \
+	}
+
+#define NLMSG_DESC_INVALID(_name) \
+	[_name] = { \
+		.name = #_name, \
+	}
+
+/* function to pretty print a genetlink message */
+int pretty_print_genlmsg(const struct nlmsghdr *nlhdr,
+			 const struct pretty_nlmsg_desc *desc,
+			 unsigned int ndesc, unsigned int err_offset);
+
+#endif /* ETHTOOL_NETLINK_PRETTYMSG_H__ */
