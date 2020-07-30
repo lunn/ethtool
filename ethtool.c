lunn/ethtool

commit 8bb9a04002a38fd24be122fed35420fee589bc6e

ethtool.c: Report transceiver correctly
After the transition to the extended link settings ioctl API, the
kernel, up until commit 9b3004953503462a4fab31b85e44ae446d48f0bd
("ethtool: drop get_settings and set_settings callbacks") would support
the legacy link settings operation and report the transceiver type.
After this commit, we lost that information although the Linux PHY
library populates it correctly even with get_link_ksettings() method.

Ensure that we report the transceiver type correctly for such cases.

Fixes: 33133abf3b77 ("ethtool.c: add support for ETHTOOL_xLINKSETTINGS ioctls")
Signed-off-by: Florian Fainelli <f.fainelli@gmail.com>
Signed-off-by: John W. Linville <linville@tuxdriver.com>

ethtool.c
@@ -2506,8 +2506,8 @@ do_ioctl_glinksettings(struct cmd_context *ctx)
 	if (link_usettings == NULL)
 		return NULL;
 
-	/* keep transceiver 0 */
 	memcpy(&link_usettings->base, &ecmd.req, sizeof(link_usettings->base));
+	link_usettings->deprecated.transceiver = ecmd.req.transceiver;
 
 	/* copy link mode bitmaps */
 	u32_offs = 0;

commit bef780467fa7aa95dca3ed5cc3ebb8bec5882f08

ioctl: do not pass transceiver value back to kernel
While we want to report transceiver value in "ethtool <dev>" output, we
must not return nonzero value provided in ETHTOOL_GLINKSETTINGS reply back
to kernel in ETHTOOL_SLINKSETTINGS request.

Fixes: 8bb9a04002a3 ("ethtool.c: Report transceiver correctly")
Reported-by: Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
Signed-off-by: Michal Kubecek <mkubecek@suse.cz>

ethtool.c
@@ -2906,6 +2906,8 @@ static int do_sset(struct cmd_context *ctx)
 		struct ethtool_link_usettings *link_usettings;
 
 		link_usettings = do_ioctl_glinksettings(ctx);
+		memset(&link_usettings->deprecated, 0,
+		       sizeof(link_usettings->deprecated));
 		if (link_usettings == NULL)
 			link_usettings = do_ioctl_gset(ctx);
 		if (link_usettings == NULL) {