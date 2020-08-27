lunn/ethtool

commit bef780467fa7aa95dca3ed5cc3ebb8bec5882f08

ioctl: do not pass transceiver value back to kernel
While we want to report transceiver value in "ethtool <dev>" output, we
must not return nonzero value provided in ETHTOOL_GLINKSETTINGS reply back
to kernel in ETHTOOL_SLINKSETTINGS request.

Fixes: 8bb9a04002a3 ("ethtool.c: Report transceiver correctly")
Reported-by: Ali MJ Al-Nasrawy <alimjalnasrawy@gmail.com>
Signed-off-by: Michal Kubecek <mkubecek@suse.cz>

ethtool.c
@@ -2906,6 +2906,8 @@ static int do_sset(struct cmd_context *ctx)
 		struct ethtool_link_usettings *link_usettings;
 
 		link_usettings = do_ioctl_glinksettings(ctx);
+		memset(&link_usettings->deprecated, 0,
+		       sizeof(link_usettings->deprecated));
 		if (link_usettings == NULL)
 			link_usettings = do_ioctl_gset(ctx);
 		if (link_usettings == NULL) {

commit cf12872ebe7d8fac2088e7d2cd5e2a0a5f03499d

ioctl: only memset non-NULL link settings
In commit bef780467fa ("ioctl: do not pass transceiver value back to
kernel") a regression slipped in. If we have a kernel that does not
support the ETHTOOL_xLINKSETTINGS API, then the do_ioctl_glinksettings()
function will return a NULL pointer.

Hence before memset'ing the pointer to zero we must first check it is
valid, as NULL return is perfectly fine when running on old kernels.

Fixes: bef780467fa7 ("ioctl: do not pass transceiver value back to kernel")
Signed-off-by: Hans-Christian Noren Egtvedt <hegtvedt@cisco.com>
Signed-off-by: Michal Kubecek <mkubecek@suse.cz>

ethtool.c
@@ -3048,10 +3048,11 @@ static int do_sset(struct cmd_context *ctx)
 		struct ethtool_link_usettings *link_usettings;
 
 		link_usettings = do_ioctl_glinksettings(ctx);
-		memset(&link_usettings->deprecated, 0,
-		       sizeof(link_usettings->deprecated));
 		if (link_usettings == NULL)
 			link_usettings = do_ioctl_gset(ctx);
+		else
+			memset(&link_usettings->deprecated, 0,
+			       sizeof(link_usettings->deprecated));
 		if (link_usettings == NULL) {
 			perror("Cannot get current device settings");
 			err = -1;